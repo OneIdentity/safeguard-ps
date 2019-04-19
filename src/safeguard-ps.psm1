f82130efee2725ca675c46184fc787a9051421d6
petrsnd <petrsnd@gmail.com>
2019-04-19 10:24:54 -0600

    Condition was slightly off

--- src/safeguard-ps.psm1
+++ src/safeguard-ps.psm1
@@ -1152,7 +1152,7 @@ function Invoke-SafeguardMethod
     $ErrorActionPreference = "Stop"
     if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
 
-    if (-not ($PSBoundParameters.ContainsKey("Version") -and $SafeguardSession))
+    if (-not ($PSBoundParameters.ContainsKey("Version")) -and $SafeguardSession)
     {
         # Use version from the connection if included in the session
         # Connect-Safeguard will automatically downgrade if v2 was required to call LoginResponse
