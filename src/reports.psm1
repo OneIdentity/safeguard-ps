9b04723091a3a93bf92906aa391de9f6b705e837
petrsnd <petrsnd@gmail.com>
2019-05-18 00:25:04 -0600

    New entitlement report cmdlet

--- src/reports.psm1
+++ src/reports.psm1
@@ -565,3 +565,77 @@ function Get-SafeguardReportDailyPasswordChangeSuccess
         "AssetPartitionId,AssetPartitionName,ProfileId,ProfileName,SyncGroupId,SyncGroupName,ApplianceId,ApplianceName") `
         -OutFile $local:OutFile -Excel:$Excel
 }
+
+<#
+.SYNOPSIS
+Generates user entitlement report for a set of users in Safeguard via the Web API.
+
+.DESCRIPTION
+User entitlement report is a report of what accounts can be accessed by a set of users.
+
+.PARAMETER Appliance
+IP address or hostname of a Safeguard appliance.
+
+.PARAMETER AccessToken
+A string containing the bearer token to be used with Safeguard Web API.
+
+.PARAMETER Insecure
+Ignore verification of Safeguard appliance SSL certificate.
+
+.PARAMETER UserList
+An integer containing the ID of the access policy to get or a string containing the name.
+
+.INPUTS
+None.
+
+.OUTPUTS
+JSON response from Safeguard Web API.
+
+.EXAMPLE
+Get-SafeguardReportUserEntitlement -AccessToken $token -Appliance 10.5.32.54 -Insecure
+
+.EXAMPLE
+Get-SafeguardReportUserEntitlement testUser1,testUser2
+
+.EXAMPLE
+Get-SafeguardReportUserEntitlement 123
+#>
+function Get-SafeguardReportUserEntitlement
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$Appliance,
+        [Parameter(Mandatory=$false)]
+        [object]$AccessToken,
+        [Parameter(Mandatory=$false)]
+        [switch]$Insecure,
+        [Parameter(Mandatory=$false,Position=0)]
+        [object[]]$UserList
+    )
+
+    $ErrorActionPreference = "Stop"
+    if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+    Import-Module -Name "$PSScriptRoot\sg-utilities.psm1" -Scope Local
+    if (-not (Test-SafeguardMinVersionInternal -Appliance $Appliance -Insecure:$Insecure -MinVersion "2.7"))
+    {
+        throw "This cmdlet requires Safeguard version 2.7 or greater"
+    }
+
+    if ($UserList)
+    {
+        [object[]]$local:Users = $null
+        foreach ($local:User in $UserList)
+        {
+            $local:ResolvedUser = (Get-SafeguardUser -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure -UserToGet $User)
+            $local:Users += $($local:ResolvedUser).Id
+        }
+        Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core GET "Reports/Entitlements/UserEntitlements" `
+            -Parameters @{ userIds = ($Users -join ",") }
+    }
+    else
+    {
+        Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core GET "Reports/Entitlements/UserEntitlements"
+    }
+}
