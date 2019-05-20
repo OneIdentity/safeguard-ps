cd8760de48e5028d478f86546e9403108fc1c5b5
petrsnd <petrsnd@gmail.com>
2019-05-20 13:30:15 -0600

    Adding docker convenience scripts for PowerShell

---
+++ invoke-docker-build.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$false,Position=0)]
+    [string]$ImageType = "alpine"
+)
+
+$ErrorActionPreference = "Stop"
+if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+Import-Module -Name "$PSScriptRoot\docker-include.psm1" -Scope Local
+
+$ImageType = $ImageType.ToLower()
+$SafeguardDockerFile = (Get-SafeguardDockerFile $ImageType)
+
+if (-not (Test-Command "docker"))
+{
+    throw "Unabled to find docker command. Is docker installed on this machine?"
+}
+
+if (Invoke-Expression "docker images -q safeguard-ps:$ImageType")
+{
+    Write-Host "Cleaning up the old image: safeguard-ps:$ImageType ..."
+    & docker rmi --force "safeguard-ps:$ImageType"
+}
+
+Write-Host "Building a new image: safeguard-ps:$ImageType ..."
+& docker build --no-cache -t "safeguard-ps:$ImageType" -f "$SafeguardDockerFile" "$PSScriptRoot"
