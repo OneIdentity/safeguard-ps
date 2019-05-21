45d796bc3c7a608b57cdf94b95470647d4253dd0
petrsnd <petrsnd@gmail.com>
2019-05-21 09:39:18 -0600

    docker convenience scripts for bash

--- run-docker.sh
+++ run-docker.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-
-print_usage()
-{
-    cat <<EOF
-USAGE: run-docker.sh [imagetype] [-h]
-
-  -h  Show help and exit
-
-imagetype should be one of the following: 
-
-  'ubuntu', 'ubuntu18.04', 'ubuntu16.04'
-  'centos', 'centos7'
-  'alpine', 'alpine3.8'
-  'opensuse', 'opensuse42.3'
-  'fedora', 'fedora28'
-
-EOF
-    exit 0
-}
+trap "exit 1" TERM
+export TOP_PID=$$
 
 if [ -z "$1" ]; then
     ImageType=alpine
@@ -25,34 +8,14 @@ else
     ImageType=$1
 fi
 
-case $ImageType in
-ubuntu | ubuntu18.04)
-    DockerFile="Dockerfile_ubuntu18.04"
-    ;;
-ubuntu16.04)
-    DockerFile="Dockerfile_ubuntu16.04"
-    ;;
-centos | centos7)
-    DockerFile="Dockerfile_centos7"
-    ;;
-alpine | alpine3.8)
-    DockerFile="Dockerfile_alpine3.8"
-    ;;
-opensuse | opensuse42.3)
-    DockerFile="Dockerfile_opensuse42.3"
-    ;;
-fedora | fedora28)
-    DockerFile="Dockerfile_fedora28"
-    ;;
-*)
-    print_usage
-    ;;
-esac
-
 ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "$ScriptDir/docker-include.sh"
+
+DockerFile=`get_safeguard_dockerfile $ImageType`
 
 echo "Rebuilding the image: safeguard-ps:$ImageType ..."
 $ScriptDir/build-docker.sh $ImageType
 
 echo "Running the image: safeguard-ps:$ImageType ..."
 docker run -it "safeguard-ps:$ImageType"
+
