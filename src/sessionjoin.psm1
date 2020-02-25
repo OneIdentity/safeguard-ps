ff719b5fd2a0edae74a85e54d24e9e7478584fe2
petrsnd <petrsnd@gmail.com>
2020-02-25 14:21:19 -0700

    Out-Null was short-circuiting transactions

--- src/sessionjoin.psm1
+++ src/sessionjoin.psm1
@@ -89,7 +89,7 @@ function Connect-Sps
     Remove-Variable -Scope local PasswordPlainText
 
     Invoke-RestMethod -Uri "https://$SessionMaster/api/authentication" -SessionVariable HttpSession `
-        -Headers @{ Authorization = ("Basic {0}" -f $local:BasicAuthInfo) } | Out-Null
+        -Headers @{ Authorization = ("Basic {0}" -f $local:BasicAuthInfo) } | Write-Verbose
     Remove-Variable -Scope local BasicAuthInfo
 
     $HttpSession
@@ -271,7 +271,7 @@ function Set-SafeguardSessionCluster
     $local:SessionCluster.UseHostNameForLaunch = ([bool]$UseDns)
 
     Invoke-SafeguardMethod -Appliance $Appliance -AccessToken $AccessToken -Insecure:$Insecure Core `
-        PUT "Cluster/SessionModules/$($local:SessionCluster.Id)" -Body $local:SessionCluster | Out-Null
+        PUT "Cluster/SessionModules/$($local:SessionCluster.Id)" -Body $local:SessionCluster | Write-Verbose
 
     Get-SafeguardSessionCluster -Appliance $Appliance -AccessToken $AccessToken -Insecure:$Insecure $SessionMaster -AllFields:$AllFields
 }
@@ -418,18 +418,18 @@ function Join-SafeguardSessionCluster
                 try
                 {
                     Invoke-RestMethod -WebSession $HttpSession -Method Post -Uri "https://$SessionMaster/api/transaction" `
-                        -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } | Out-Null
+                        -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } | Write-Verbose
 
                     Invoke-RestMethod -WebSession $HttpSession -Method Put -Uri "https://$SessionMaster/api/configuration/local_services/cluster" `
                         -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } -Body (ConvertTo-Json -InputObject @{
                             enabled = $true;
                             listen_address = $local:NicRef
-                        }) | Out-Null
+                        }) | Write-Verbose
 
                     Invoke-RestMethod -WebSession $HttpSession -Method Put -Uri "https://$SessionMaster/api/transaction" `
                         -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } -Body (ConvertTo-Json -InputObject @{
                             status = "commit"
-                        }) | Out-Null
+                        }) | Write-Verbose
 
                     Start-Sleep -Seconds 10
                 }
@@ -437,7 +437,7 @@ function Join-SafeguardSessionCluster
                 {
                     try
                     {
-                        Invoke-RestMethod -WebSession $HttpSession -Method Delete -Uri "https://$SessionMaster/api/transaction" | Out-Null
+                        Invoke-RestMethod -WebSession $HttpSession -Method Delete -Uri "https://$SessionMaster/api/transaction" | Write-Verbose
                     }
                     catch {}
                 }
@@ -454,7 +454,7 @@ function Join-SafeguardSessionCluster
         # Make sure this node is a session master
         try
         {
-            Invoke-RestMethod -WebSession $HttpSession -Method Get -Uri "https://$SessionMaster/api/cluster/status" | Out-Null
+            Invoke-RestMethod -WebSession $HttpSession -Method Get -Uri "https://$SessionMaster/api/cluster/status" | Write-Verbose
         }
         catch
         {
@@ -466,15 +466,15 @@ function Join-SafeguardSessionCluster
                 try
                 {
                     Invoke-RestMethod -WebSession $HttpSession -Method Post -Uri "https://$SessionMaster/api/transaction" `
-                        -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } | Out-Null
+                        -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } | Write-Verbose
 
                     Invoke-RestMethod -WebSession $HttpSession -Method Post -Uri "https://$SessionMaster/api/cluster/promote" `
-                        -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } | Out-Null
+                        -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } | Write-Verbose
 
                     Invoke-RestMethod -WebSession $HttpSession -Method Put -Uri "https://$SessionMaster/api/transaction" `
                         -Headers @{ "Accept" = "application/json"; "Content-type" = "application/json" } -Body (ConvertTo-Json -InputObject @{
                             status = "commit"
-                        }) | Out-Null
+                        }) | Write-Verbose
 
                     Start-Sleep -Seconds 10
                 }
@@ -482,7 +482,7 @@ function Join-SafeguardSessionCluster
                 {
                     try
                     {
-                        Invoke-RestMethod -WebSession $HttpSession -Method Delete -Uri "https://$SessionMaster/api/transaction" | Out-Null
+                        Invoke-RestMethod -WebSession $HttpSession -Method Delete -Uri "https://$SessionMaster/api/transaction" | Write-Verbose
                     }
                     catch {}
                 }
@@ -503,7 +503,7 @@ function Join-SafeguardSessionCluster
                 spp = $Appliance;
                 spp_api_token = $AccessToken;
                 spp_cert_chain = $local:SppCertData
-            }) | Out-Null
+            }) | Write-Verbose
 
         Start-Sleep -Seconds 30
 
@@ -582,7 +582,7 @@ function Split-SafeguardSessionCluster
     }
 
     Invoke-SafeguardMethod -Appliance $Appliance -AccessToken $AccessToken -Insecure:$Insecure Core `
-        DELETE "Cluster/SessionModules/$($local:SessionCluster.Id)" | Out-Null
+        DELETE "Cluster/SessionModules/$($local:SessionCluster.Id)" | Write-Verbose
 }
 
 <#
@@ -698,7 +698,7 @@ function Remove-SafeguardSessionSplitCluster
     }
 
     Invoke-SafeguardMethod -Appliance $Appliance -AccessToken $AccessToken -Insecure:$Insecure Core `
-        DELETE "Cluster/SessionModules/$($local:SessionCluster.Id)" | Out-Null
+        DELETE "Cluster/SessionModules/$($local:SessionCluster.Id)" | Write-Verbose
 }
 
 <#
