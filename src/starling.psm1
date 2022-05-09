5c38bc77959f7e8d16a603ddd5b68e9be3a57ff6
Pravinbhai-Parmar <pravinbhai.parmar@oneidentity.com>
2022-05-09 12:56:06 -0700

    Product Backlog Item 306136: safeguard-ps scripting

--- src/starling.psm1
+++ src/starling.psm1
@@ -282,6 +282,73 @@ function Remove-SafeguardStarlingSubscription
     }
 }
 
+<#
+.SYNOPSIS
+Remove Safeguard Starling from user's secondary authentication provider from safeguard users.
+
+.DESCRIPTION
+This cmdlet will remove the Starling Authentication Profider from safeguard users.
+
+.PARAMETER Appliance
+IP address or hostname of a Safeguard appliance.
+
+.PARAMETER AccessToken
+A string containing the bearer token to be used with Safeguard Web API.
+
+.PARAMETER Insecure
+Ignore verification of Safeguard appliance SSL certificate.
+
+.INPUTS
+None.
+
+.OUTPUTS
+JSON response from Safeguard Web API.
+
+.EXAMPLE
+Remove-SafeguardStarling2FA default
+
+#>
+function Remove-SafeguardStarling2FA
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$Appliance,
+        [Parameter(Mandatory=$false)]
+        [object]$AccessToken,
+        [Parameter(Mandatory=$false)]
+        [switch]$Insecure,        
+    )
+
+    if (-not $PSBoundParameters.ContainsKey("ErrorAction")) { $ErrorActionPreference = "Stop" }
+    if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+	try 
+	{
+		$local:UserIds = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core GET Users `
+										 -Parameters @{ filter = "SecondaryAuthenticationProviderTypeReferenceName in ['StarlingSubscription', 'StarlingTwoFactor']"; fields = "Id" })   
+		
+		Foreach ($Id in $local:UserIds)
+		{
+			$UserObject = (Get-SafeguardUser -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $Id)
+			
+			if($UserObject -ne $null)
+			{
+			
+				$UserObject.SecondaryAuthenticationProviderId = $null
+				$UserObject.SecondaryAuthenticationProviderName = $null
+				$UserObject.SecondaryAuthenticationProviderTypeReferenceName = "Unknown"
+				$UserObject.SecondaryAuthenticationIdentity = $null
+				
+				Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core PUT "Users/$($UserObject.Id)" -Body $UserObject
+			}
+		}
+	}
+	catch
+	{
+		Write-Verbose "Error occured while removing Starling Two Factor authentication from user(s)."
+	}
+}
 
 <#
 .SYNOPSIS
