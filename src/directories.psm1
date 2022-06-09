1590d3af9e761c1c0a95f1a26778acdede43d7e0
petrsnd <petrsnd@gmail.com>
2022-06-09 11:25:15 -0600

    directory migration cmdlet call v3 only

--- src/directories.psm1
+++ src/directories.psm1
@@ -2044,28 +2044,28 @@ function Get-SafeguardDirectoryMigrationData
     }
 
     $local:DirectoriesCreated = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure `
-        Core GET "AuditLog/ObjectChanges/Directory" `
+        Core GET "AuditLog/ObjectChanges/Directory" -Version 3 `
         -Parameters @{filter = "EventName eq 'DirectoryCreated'"; fields = "LogTime,EventName,ObjectName,ObjectId";
                       startDate = (Get-EntireAuditLogStartDateAsString)})
     Write-Verbose "Directories Created:"
     Write-Verbose ($local:DirectoriesCreated | Format-Table | Out-String)
 
     $local:DirectoriesDeleted = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure `
-        Core GET "AuditLog/ObjectChanges/Directory" `
+        Core GET "AuditLog/ObjectChanges/Directory" -Version 3 `
         -Parameters @{filter = "EventName eq 'DirectoryDeleted'"; fields = "LogTime,EventName,ObjectName,ObjectId";
                       startDate = (Get-EntireAuditLogStartDateAsString)})
     Write-Verbose "Directories Deleted:"
     Write-Verbose ($local:DirectoriesDeleted | Format-Table | Out-String)
 
     $local:IdentityProvidersCreated = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure `
-        Core GET "AuditLog/ObjectChanges/IdentityProvider" `
+        Core GET "AuditLog/ObjectChanges/IdentityProvider" -Version 3 `
         -Parameters @{filter = "EventName eq 'IdentityProviderCreated'"; fields = "LogTime,EventName,ObjectName,ObjectId";
                       startDate = (Get-EntireAuditLogStartDateAsString)})
     Write-Verbose "Directory Identity Providers Created:"
     Write-Verbose ($local:IdentityProvidersCreated | Format-Table | Out-String)
 
     $local:DirectoryAssetsCreated = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure `
-        Core GET "AuditLog/ObjectChanges/Asset" `
+        Core GET "AuditLog/ObjectChanges/Asset" -Version 3 `
         -Parameters @{filter = "EventName eq 'AssetCreated' and NewValue contains '`"IsDirectory`":true'";
                       fields = "LogTime,EventName,ObjectName,ObjectId";
                       startDate = (Get-EntireAuditLogStartDateAsString)})
