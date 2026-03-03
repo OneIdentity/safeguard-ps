0c520161aa28c2216cf8b53f7fc98a9b72b05887
petrsnd <petrsnd@gmail.com>
2026-03-03 15:46:42 -0700

    Starting point for supporting access policy creation

--- src/policies.psm1
+++ src/policies.psm1
@@ -128,6 +128,8 @@ function Resolve-SafeguardAccessPolicyId
         [object]$AccessToken,
         [Parameter(Mandatory=$false)]
         [switch]$Insecure,
+        [Parameter(Mandatory=$false)]
+        [int]$EntitlementId,
         [Parameter(Mandatory=$true,Position=0)]
         [object]$AccessPolicy
     )
@@ -142,17 +144,27 @@ function Resolve-SafeguardAccessPolicyId
 
     if (-not ($AccessPolicy -as [int]))
     {
+        $local:Filter = "Name ieq '$AccessPolicy'"
+        if ($PSBoundParameters.ContainsKey("EntitlementId"))
+        {
+            $local:Filter = "RoleId eq $EntitlementId and $($local:Filter)"
+        }
         try
         {
             $local:AccessPolicies = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core GET AccessPolicies `
-                                 -Parameters @{ filter = "Name ieq '$AccessPolicy'" })
+                                 -Parameters @{ filter = $local:Filter })
         }
         catch
         {
             Write-Verbose $_
             Write-Verbose "Caught exception with ieq filter, trying with q parameter"
+            $local:Params = @{ q = $AccessPolicy }
+            if ($PSBoundParameters.ContainsKey("EntitlementId"))
+            {
+                $local:Params["filter"] = "RoleId eq $EntitlementId"
+            }
             $local:AccessPolicies = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core GET AccessPolicies `
-                                 -Parameters @{ q = $AccessPolicy })
+                                 -Parameters $local:Params)
         }
         if (-not $local:AccessPolicies)
         {
@@ -968,4 +980,376 @@ function Remove-SafeguardUserLinkedAccount
         Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core `
             PUT "Users/$local:UserId/LinkedPolicyAccounts" -Body $local:LinkedAccountsToSet
     }
+}
+
+# Helper to build ScopeItems array from convenience parameters
+function Resolve-SafeguardAccessPolicyScopeItems
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$Appliance,
+        [Parameter(Mandatory=$false)]
+        [object]$AccessToken,
+        [Parameter(Mandatory=$false)]
+        [switch]$Insecure,
+        [Parameter(Mandatory=$false)]
+        [object[]]$ScopeAccounts,
+        [Parameter(Mandatory=$false)]
+        [object[]]$ScopeAssets,
+        [Parameter(Mandatory=$false)]
+        [object[]]$ScopeAccountGroups,
+        [Parameter(Mandatory=$false)]
+        [object[]]$ScopeAssetGroups
+    )
+
+    if (-not $PSBoundParameters.ContainsKey("ErrorAction")) { $ErrorActionPreference = "Stop" }
+    if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+    [object[]]$local:ScopeItems = @()
+
+    foreach ($local:Account in $ScopeAccounts)
+    {
+        $local:AccountId = (Resolve-SafeguardPolicyAccountId -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $local:Account)
+        $local:ScopeItems += @{ Id = $local:AccountId; ScopeItemType = "Account" }
+    }
+
+    foreach ($local:Asset in $ScopeAssets)
+    {
+        $local:AssetId = (Resolve-SafeguardPolicyAssetId -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $local:Asset)
+        $local:ScopeItems += @{ Id = $local:AssetId; ScopeItemType = "Asset" }
+    }
+
+    foreach ($local:AccountGroup in $ScopeAccountGroups)
+    {
+        Import-Module -Name "$PSScriptRoot\groups.psm1" -Scope Local
+        $local:GroupId = (Get-SafeguardAccountGroup -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $local:AccountGroup).Id
+        $local:ScopeItems += @{ Id = $local:GroupId; ScopeItemType = "AccountGroup" }
+    }
+
+    foreach ($local:AssetGroup in $ScopeAssetGroups)
+    {
+        Import-Module -Name "$PSScriptRoot\groups.psm1" -Scope Local
+        $local:GroupId = (Get-SafeguardAssetGroup -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $local:AssetGroup).Id
+        $local:ScopeItems += @{ Id = $local:GroupId; ScopeItemType = "AssetGroup" }
+    }
+
+    $local:ScopeItems
+}
+
+<#
+.SYNOPSIS
+Add an access policy to an entitlement in Safeguard via the Web API.
+
+.DESCRIPTION
+Add an access policy to an existing entitlement in Safeguard. Access policies define what
+assets and accounts can be requested and the type of access (password, session, etc.).
+
+.PARAMETER Appliance
+IP address or hostname of a Safeguard appliance.
+
+.PARAMETER AccessToken
+A string containing the bearer token to be used with Safeguard Web API.
+
+.PARAMETER Insecure
+Ignore verification of Safeguard appliance SSL certificate.
+
+.PARAMETER Entitlement
+An integer containing the ID or a string containing the name of the entitlement to add the policy to.
+
+.PARAMETER Name
+The name of the new access policy.
+
+.PARAMETER AccessRequestType
+The type of access being granted (Password, RemoteDesktop, Ssh, Telnet, SshKey, RemoteDesktopApplication, ApiKey, File).
+
+.PARAMETER Description
+A string containing a description of the access policy.
+
+.PARAMETER ScopeAccounts
+An array of account IDs or names to include in the policy scope.
+
+.PARAMETER ScopeAssets
+An array of asset IDs or names to include in the policy scope.
+
+.PARAMETER ScopeAccountGroups
+An array of account group IDs or names to include in the policy scope.
+
+.PARAMETER ScopeAssetGroups
+An array of asset group IDs or names to include in the policy scope.
+
+.PARAMETER AccessPolicyObject
+An object containing the access policy to create. Use this for advanced configuration
+(approver sets, session properties, hourly restrictions, etc.).
+
+.INPUTS
+None.
+
+.OUTPUTS
+JSON response from Safeguard Web API.
+
+.EXAMPLE
+Add-SafeguardAccessPolicy -Entitlement "Lab Administrator" -Name "SSH Access" -AccessRequestType Ssh
+
+.EXAMPLE
+Add-SafeguardAccessPolicy -Entitlement 5 -Name "Password Access" -AccessRequestType Password -ScopeAccounts "root","admin"
+
+.EXAMPLE
+Add-SafeguardAccessPolicy -AccessPolicyObject $policyObj
+#>
+function Add-SafeguardAccessPolicy
+{
+    [CmdletBinding(DefaultParameterSetName="Attributes")]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$Appliance,
+        [Parameter(Mandatory=$false)]
+        [object]$AccessToken,
+        [Parameter(Mandatory=$false)]
+        [switch]$Insecure,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$true,Position=0)]
+        [object]$Entitlement,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$true,Position=1)]
+        [string]$Name,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$true,Position=2)]
+        [ValidateSet("Password","RemoteDesktop","Ssh","Telnet","SshKey","RemoteDesktopApplication","ApiKey","File",IgnoreCase=$true)]
+        [string]$AccessRequestType,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [string]$Description,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAccounts,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAssets,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAccountGroups,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAssetGroups,
+        [Parameter(ParameterSetName="Object",Mandatory=$true,ValueFromPipeline=$true)]
+        [object]$AccessPolicyObject
+    )
+
+    if (-not $PSBoundParameters.ContainsKey("ErrorAction")) { $ErrorActionPreference = "Stop" }
+    if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+    if ($PsCmdlet.ParameterSetName -eq "Object")
+    {
+        if (-not $AccessPolicyObject)
+        {
+            throw "AccessPolicyObject must not be null"
+        }
+    }
+    else
+    {
+        Import-Module -Name "$PSScriptRoot\entitlements.psm1" -Scope Local
+        $local:EntitlementId = Resolve-SafeguardEntitlementId -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $Entitlement
+
+        $AccessPolicyObject = @{
+            Name = $Name;
+            RoleId = $local:EntitlementId;
+            AccessRequestProperties = @{
+                AccessRequestType = $AccessRequestType
+            }
+        }
+
+        if ($PSBoundParameters.ContainsKey("Description")) { $AccessPolicyObject.Description = $Description }
+
+        if ($PSBoundParameters.ContainsKey("ScopeAccounts") -or $PSBoundParameters.ContainsKey("ScopeAssets") -or `
+            $PSBoundParameters.ContainsKey("ScopeAccountGroups") -or $PSBoundParameters.ContainsKey("ScopeAssetGroups"))
+        {
+            $AccessPolicyObject.ScopeItems = (Resolve-SafeguardAccessPolicyScopeItems -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure `
+                -ScopeAccounts $ScopeAccounts -ScopeAssets $ScopeAssets -ScopeAccountGroups $ScopeAccountGroups -ScopeAssetGroups $ScopeAssetGroups)
+        }
+    }
+
+    Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core POST AccessPolicies -Body $AccessPolicyObject
+}
+
+<#
+.SYNOPSIS
+Remove an access policy from Safeguard via the Web API.
+
+.DESCRIPTION
+Remove an access policy from Safeguard. The access policy will be permanently deleted.
+
+.PARAMETER Appliance
+IP address or hostname of a Safeguard appliance.
+
+.PARAMETER AccessToken
+A string containing the bearer token to be used with Safeguard Web API.
+
+.PARAMETER Insecure
+Ignore verification of Safeguard appliance SSL certificate.
+
+.PARAMETER PolicyToDelete
+An integer containing the ID or a string containing the name of the access policy to delete.
+
+.PARAMETER Entitlement
+An integer containing the ID or a string containing the name of the entitlement to qualify
+the access policy lookup in case multiple policies share the same name.
+
+.INPUTS
+None.
+
+.OUTPUTS
+JSON response from Safeguard Web API.
+
+.EXAMPLE
+Remove-SafeguardAccessPolicy "SSH Access"
+
+.EXAMPLE
+Remove-SafeguardAccessPolicy "SSH Access" -Entitlement "Lab Administrator"
+
+.EXAMPLE
+Remove-SafeguardAccessPolicy 123
+#>
+function Remove-SafeguardAccessPolicy
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$Appliance,
+        [Parameter(Mandatory=$false)]
+        [object]$AccessToken,
+        [Parameter(Mandatory=$false)]
+        [switch]$Insecure,
+        [Parameter(Mandatory=$true,Position=0)]
+        [object]$PolicyToDelete,
+        [Parameter(Mandatory=$false)]
+        [object]$Entitlement
+    )
+
+    if (-not $PSBoundParameters.ContainsKey("ErrorAction")) { $ErrorActionPreference = "Stop" }
+    if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+    $local:ResolveParams = @{}
+    if ($PSBoundParameters.ContainsKey("Entitlement"))
+    {
+        Import-Module -Name "$PSScriptRoot\entitlements.psm1" -Scope Local
+        $local:ResolveParams["EntitlementId"] = (Resolve-SafeguardEntitlementId -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $Entitlement)
+    }
+
+    $local:AccessPolicyId = Resolve-SafeguardAccessPolicyId -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure @local:ResolveParams $PolicyToDelete
+    Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core DELETE "AccessPolicies/$($local:AccessPolicyId)"
+}
+
+<#
+.SYNOPSIS
+Edit an existing access policy in Safeguard via the Web API.
+
+.DESCRIPTION
+Edit an existing access policy in Safeguard. Access policies define what assets and accounts
+can be requested and the type of access (password, session, etc.).
+
+.PARAMETER Appliance
+IP address or hostname of a Safeguard appliance.
+
+.PARAMETER AccessToken
+A string containing the bearer token to be used with Safeguard Web API.
+
+.PARAMETER Insecure
+Ignore verification of Safeguard appliance SSL certificate.
+
+.PARAMETER PolicyToEdit
+An integer containing the ID or a string containing the name of the access policy to edit.
+
+.PARAMETER Name
+A string containing the new name for the access policy.
+
+.PARAMETER Description
+A string containing the new description for the access policy.
+
+.PARAMETER AccessRequestType
+The type of access being granted (Password, RemoteDesktop, Ssh, Telnet, SshKey, RemoteDesktopApplication, ApiKey, File).
+
+.PARAMETER ScopeAccounts
+An array of account IDs or names to set as the policy scope (replaces existing scope accounts).
+
+.PARAMETER ScopeAssets
+An array of asset IDs or names to set as the policy scope (replaces existing scope assets).
+
+.PARAMETER ScopeAccountGroups
+An array of account group IDs or names to set as the policy scope (replaces existing scope account groups).
+
+.PARAMETER ScopeAssetGroups
+An array of asset group IDs or names to set as the policy scope (replaces existing scope asset groups).
+
+.PARAMETER AccessPolicyObject
+An object containing the existing access policy with desired properties set.
+
+.INPUTS
+None.
+
+.OUTPUTS
+JSON response from Safeguard Web API.
+
+.EXAMPLE
+Edit-SafeguardAccessPolicy -PolicyToEdit "SSH Access" -Description "Updated description"
+
+.EXAMPLE
+Edit-SafeguardAccessPolicy -PolicyToEdit 123 -ScopeAccounts "root","admin"
+
+.EXAMPLE
+$obj = Get-SafeguardAccessPolicy "SSH Access"; $obj.Description = "New desc"; Edit-SafeguardAccessPolicy -AccessPolicyObject $obj
+#>
+function Edit-SafeguardAccessPolicy
+{
+    [CmdletBinding(DefaultParameterSetName="Attributes")]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [string]$Appliance,
+        [Parameter(Mandatory=$false)]
+        [object]$AccessToken,
+        [Parameter(Mandatory=$false)]
+        [switch]$Insecure,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$true,Position=0)]
+        [object]$PolicyToEdit,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [string]$Name,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [string]$Description,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [ValidateSet("Password","RemoteDesktop","Ssh","Telnet","SshKey","RemoteDesktopApplication","ApiKey","File",IgnoreCase=$true)]
+        [string]$AccessRequestType,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAccounts,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAssets,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAccountGroups,
+        [Parameter(ParameterSetName="Attributes",Mandatory=$false)]
+        [object[]]$ScopeAssetGroups,
+        [Parameter(ParameterSetName="Object",Mandatory=$true,ValueFromPipeline=$true)]
+        [object]$AccessPolicyObject
+    )
+
+    if (-not $PSBoundParameters.ContainsKey("ErrorAction")) { $ErrorActionPreference = "Stop" }
+    if (-not $PSBoundParameters.ContainsKey("Verbose")) { $VerbosePreference = $PSCmdlet.GetVariableValue("VerbosePreference") }
+
+    if ($PsCmdlet.ParameterSetName -eq "Object" -and -not $AccessPolicyObject)
+    {
+        throw "AccessPolicyObject must not be null"
+    }
+
+    if ($PsCmdlet.ParameterSetName -eq "Attributes")
+    {
+        $local:AccessPolicyId = Resolve-SafeguardAccessPolicyId -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure $PolicyToEdit
+        $AccessPolicyObject = (Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core GET "AccessPolicies/$($local:AccessPolicyId)")
+
+        if ($PSBoundParameters.ContainsKey("Name")) { $AccessPolicyObject.Name = $Name }
+        if ($PSBoundParameters.ContainsKey("Description")) { $AccessPolicyObject.Description = $Description }
+        if ($PSBoundParameters.ContainsKey("AccessRequestType"))
+        {
+            if (-not $AccessPolicyObject.AccessRequestProperties) { $AccessPolicyObject.AccessRequestProperties = @{} }
+            $AccessPolicyObject.AccessRequestProperties.AccessRequestType = $AccessRequestType
+        }
+
+        if ($PSBoundParameters.ContainsKey("ScopeAccounts") -or $PSBoundParameters.ContainsKey("ScopeAssets") -or `
+            $PSBoundParameters.ContainsKey("ScopeAccountGroups") -or $PSBoundParameters.ContainsKey("ScopeAssetGroups"))
+        {
+            $AccessPolicyObject.ScopeItems = (Resolve-SafeguardAccessPolicyScopeItems -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure `
+                -ScopeAccounts $ScopeAccounts -ScopeAssets $ScopeAssets -ScopeAccountGroups $ScopeAccountGroups -ScopeAssetGroups $ScopeAssetGroups)
+        }
+    }
+
+    Invoke-SafeguardMethod -AccessToken $AccessToken -Appliance $Appliance -Insecure:$Insecure Core PUT "AccessPolicies/$($AccessPolicyObject.Id)" -Body $AccessPolicyObject
 }
